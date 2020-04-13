4488e5525f5e928d3b89d4ab94ad222220695efa
JinMurayama <shining.hart@gmail.com>
2020-04-13 12:24:07 +0000

    位置情報に応じて店舗一覧を表示する

--- JS/main.js
+++ JS/main.js
@@ -18,3 +18,26 @@ $('.animated').waypoint({
   
   offset: '80%',
 });
+
+
+function initMap() {
+  
+  function success(position){
+    var latitude  = position.coords.latitude;//緯度
+    var longitude = position.coords.longitude;//経度
+    var latlng = new google.maps.LatLng( latitude , longitude ) ;
+    var map = new google.maps.Map(document.getElementById("googlemap"), {
+      zoom: 15,
+      center: latlng,
+    });
+    var marker = new google.maps.Marker({
+      map : map,             // 対象の地図オブジェクト
+      position : latlng,   // 緯度・経度
+    });
+  };
+  function error() {
+    //エラーの場合
+    output.innerHTML = "座標位置を取得できません";
+  };
+  navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
+};
--- JS/map.js
+++ JS/map.js
@@ -1,6 +1,53 @@
+  
+var latitude
+var longitude
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById("googlemap"), {
-    zoom: 15,
-    center: {lat:35.1253694, lng:136.9073667}
-  });
+  
+  function success(position){
+    latitude  = position.coords.latitude;//緯度
+    longitude = position.coords.longitude;//経度
+    var latlng = new google.maps.LatLng( latitude , longitude ) ;
+    var map = new google.maps.Map(document.getElementById("googlemap"), {
+      zoom: 15,
+      center: latlng,
+    });
+    var marker = new google.maps.Marker({
+      map : map,             // 対象の地図オブジェクト
+      position : latlng,   // 緯度・経度
+    });
+    
+    getResult(latitude, longitude);
+    
+  };
+  function error() {
+    //エラーの場合
+    output.innerHTML = "座標位置を取得できません";
+  };
+  
+  navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
+  
+};
+
+const showResult = result => {
+  result.rest.map( item => {
+    $("#table").append(`<tr><td>${item.name}</td><td>${item.opentime}</td></tr>`)
+  })
 }
+
+
+const getResult = (lati, longi) => {
+  const url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+  console.log(lati);
+  console.log(longi);
+  const params = {
+    keyid: "",
+    latitude: lati,
+    longitude: longi,
+    range: 3
+  }
+  params.keyid ="77699251ac9ac9c426b7d38ab645ea64";
+  $.getJSON( url, params, result => {
+    showResult( result )
+  });
+};
