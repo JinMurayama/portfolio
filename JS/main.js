f2757e17f6a9209897cbfa605aed20adae88c023
JinMurayama <shining.hart@gmail.com>
2020-04-16 00:21:51 +0000

    リスト一覧クリックしたら、ピンの詳細表示

--- JS/main.js
+++ JS/main.js
@@ -19,16 +19,17 @@ $('.animated').waypoint({
   offset: '80%',
 });
 
-
+var latitude
+var longitude
 
 function initMap() {
   
   function success(position){
-//    latitude  = position.coords.latitude;//緯度
-//    longitude = position.coords.longitude;//経度
+    //latitude  = position.coords.latitude;//緯度
+    //longitude = position.coords.longitude;//経度
 
-    var latitude = 35.681432
-    var longitude = 139.767103    
+    latitude = 35.681432
+    longitude = 139.767103    
     
     var latlng = new google.maps.LatLng( latitude , longitude ) ;
     var map = new google.maps.Map(document.getElementById("googlemap"), {
@@ -43,6 +44,20 @@ function initMap() {
   function error() {
     //エラーの場合
     output.innerHTML = "座標位置を取得できません";
+    
+    var latitude = 35.681432
+    var longitude = 139.767103  
+    
+    var latlng = new google.maps.LatLng( latitude , longitude ) ;
+    var map = new google.maps.Map(document.getElementById("googlemap"), {
+      zoom: 15,
+      center: latlng,
+    });
+    var marker = new google.maps.Marker({
+      map : map,             // 対象の地図オブジェクト
+      position : latlng,   // 緯度・経度
+    });
+    
   };
   navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
 };
--- JS/map.js
+++ JS/map.js
@@ -12,8 +12,8 @@ var lng;
 function initMap() {
   
   function success(position){
-//    latitude  = position.coords.latitude;//緯度
-//    longitude = position.coords.longitude;//経度
+    latitude  = position.coords.latitude;//緯度
+    longitude = position.coords.longitude;//経度
 
     latitude = 35.681432
     longitude = 139.767103
@@ -33,7 +33,9 @@ function initMap() {
     
     getResult(latitude, longitude);
     
-    addInfo(0, "東京駅");
+    infoWindow[0] = new google.maps.InfoWindow({
+      content: `<div class="shop-name">中心地</div>`
+    });
     
     // clickイベントを取得するListenerを追加
     marker[0].addListener('click', () => {
@@ -53,7 +55,9 @@ function initMap() {
         },
       
       });
-      addInfo(0, "中心地");
+      infoWindow[0] = new google.maps.InfoWindow({
+        content: `<div class="shop-name">中心地</div>`
+      });
       // clickイベントを取得するListenerを追加
       marker[0].addListener('click', () => {
         infoWindow[0].open(map, marker[0]);
@@ -65,21 +69,75 @@ function initMap() {
   function error() {
     //エラーの場合
     output.innerHTML = "座標位置を取得できません";
+    
+    latitude = 35.681432
+    longitude = 139.767103
+    latlng = new google.maps.LatLng( latitude , longitude ) ;
+    map = new google.maps.Map(document.getElementById("googlemap"), {
+      zoom: 15,
+      center: latlng,
+    });
+    marker[0] = new google.maps.Marker({
+      map : map,             // 対象の地図オブジェクト
+      position : latlng,   // 緯度・経度
+      icon : {
+        url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
+      },
+      
+    });
+    
+    getResult(latitude, longitude);
+    
+    infoWindow[0] = new google.maps.InfoWindow({
+      content: `<div class="shop-name">東京駅</div>`
+    });
+    
+    // clickイベントを取得するListenerを追加
+    marker[0].addListener('click', () => {
+      infoWindow[0].open(map, marker[0]);
+    });
+    
+    google.maps.event.addListener(map, 'dragend', function(e) {
+      checkLatLng();
+      $("#googlemap > div > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(4)").empty()
+      latlng = new google.maps.LatLng( lat , lng ) ;
+      map.center = latlng;
+      marker[0] = new google.maps.Marker({
+        map : map,             // 対象の地図オブジェクト
+        position : latlng,   // 緯度・経度
+        icon : {
+          url: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
+        },
+      
+      });
+
+      infoWindow[0] = new google.maps.InfoWindow({
+        content: `<div class="shop-name">東京駅</div>`
+      });
+
+      // clickイベントを取得するListenerを追加
+      marker[0].addListener('click', () => {
+        infoWindow[0].open(map, marker[0]);
+      });
+      getResult(lat, lng);
+    });
   };
   
   navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
   
 };
 
-const addInfo = (i, name) => {
+const addInfo = (i, name, image, url) => {
   infoWindow[i] = new google.maps.InfoWindow({
-    content: `<div class="shop-name">${name}</div>`
+    content: `<img src='${image}'>
+    <div class="shop-name">${name}</div>
+    <button class="btn btn-primary map-button" <input type="button" onClick="window.open('${url}','_blank','')" type="button">お店の詳細に行く</button>
+    `
   });
 }
 
 const showGooglemap = (i, name, lati, longi) => {
   var latlng = new google.maps.LatLng( lati, longi );
-  clearResult();
   marker[i] = new google.maps.Marker({
     map:map, 
     position:latlng,
@@ -93,23 +151,35 @@ const showGooglemap = (i, name, lati, longi) => {
 }
 
 const clearResult = () => {
-  $("tbody").remove();
+  $("tbody").empty();
 
 }
 
 const showResult = result => {
   let i = 1;
+  clearResult();
   result.rest.map( item => {
-    $("#table").append(`<tr><td>${item.name}</td><td>${item.opentime}</td></tr>`)
-    addInfo(i, item.name);
+    $("#table").append(`<tr><td class="shops-name" id="${i}">${item.name}</td><td>${item.opentime}</td></tr>`)
+    
+    addInfo(i, item.name, item.image_url.shop_image1, item.url);
+    console.log(item.image_url.shop_image1);
     showGooglemap(i, item.name, item.latitude, item.longitude);
     markerData[i]={
-      nane:item.name,
+      name:item.name,
       lat:item.latitude,
       lng:item.longitude,
+      image:item.image_url.shop_image1,
+      url:item.url,
     }
-    console.log(i);
-    console.log(markerData[i]);
+    
+    //イベントリスナー追加
+    $(`#${i}`).on('click', (e) => {
+      var shop_id = e.target.id;
+      infoWindow[shop_id].open(map, marker[shop_id]);
+    });
+    
+    //    console.log(i);
+    console.log(markerData[i].name);
     i++;
   })
 
@@ -117,7 +187,6 @@ const showResult = result => {
 
 
 const getResult = (lati, longi) => {
-  clearResult();
   const url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
   const params = {
     keyid: "",
